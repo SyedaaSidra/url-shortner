5899a1248239f704837c7cde276003952356b4ba
sidraRashdi <rashdimuntaha557@gmail.com>
2025-07-03 03:16:13 +0500

    feat: start the server with express

--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ const express = require("express");
 const mongoConnect = require("./db");
 require("dotenv").config();
 
+const app = express();
 mongoConnect();
+
+PORT = process.env.PORT;
+app.use(express.json);
+app.listen(PORT, () => {
+  console.log(`server running on port: ${PORT}`);
+});
