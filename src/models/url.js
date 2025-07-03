b6b237fca090bc0674d9e21819ecd9b5e7286a67
sidraRashdi <rashdimuntaha557@gmail.com>
2025-07-03 05:55:31 +0500

    create schema using mongoose ODM

---
+++ src/models/url.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+  originalUrl: { type: String, required: true },
+  shortCode: { type: String, required: true, unique: true },
+  createdAt: { type: Date, default: Date.now },
+  expiredAt: { type: Date },
+  lastVisted: { type: Date },
+  Visited: { type: Number, default: 0 },
+});
+module.exports = mongoose.model("Url", schema);
